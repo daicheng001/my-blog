c79c01b4bf57ffff5b87784eab87c9666c744c62
戴程 <757214868@qq.com>
2021-04-21 08:48:25 +0800

    fix: 添加首页导航

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,15 @@ const styles = require("../styles/home.styl")
 const Home = () => {
   return (
       <Layout title="首页" description="首页">
-        <div>my-blog</div>
+        <div className={ styles["nvabar"] }>
+          <div className={ styles["title"] }></div>
+          <ul className={ styles["nav"] }>
+            <li className={ styles["index"] }><a href="/">首页</a></li>
+            <li className={ styles["about"] }><a href="/about">关于</a></li>
+            <li className={ styles["github"] }><a href="/github">github</a></li>
+            <li className={ styles["concat"] }><a href="/concat">联系</a></li>
+          </ul>
+        </div>
       </Layout>
   )
 }
