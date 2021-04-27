890577d182ca655abd1cc8264359796fc0b1f88c
戴程 <757214868@qq.com>
2021-04-27 17:28:19 +0800

    fix: 更新首页

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,11 +1,14 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { Helmet } from "react-helmet"
 import { graphql, withPrefix, useStaticQuery } from "gatsby"
 import { siteMetadata } from "../../../gatsby-config"
 import styles from '../../styles/index.styl'
+import classnames from 'classnames'
 import { Row, Col } from 'antd'
+import classNames from "classnames"
 
 const Layout = ({ title, description, children }) => {
+  const navbarRef = useRef(null)
   const data = useStaticQuery(graphql`
     query HeadingQuery {
       site {
@@ -16,6 +19,19 @@ const Layout = ({ title, description, children }) => {
       }
     }
   `)
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      console.dir(navbarRef.current)
+      navbarRef.current.classList.add(`${styles["navbarFix"]}`)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+    return (() => {
+      window.removeEventListener("scroll", handleScroll)
+    })
+  })
   return (
     <>
       <Helmet>
@@ -24,8 +40,8 @@ const Layout = ({ title, description, children }) => {
         <meta name="keywords" content=""/>
       </Helmet>
       <div>
-        <div className={ styles["nvabar"] }>
-          <div className={ styles["nav_header"] }>
+        <div className={ `${styles["nvabar"]}` }>
+          <div ref={navbarRef} className={ styles["nav_header"] }>
             <div className={ styles["title"] }></div>
             <ul className={ styles["nav"] }>
               <li className={ styles["index"] }><a href="/">home</a></li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,38 +5,72 @@ const Home = () => {
   const data = [
     {
       title: "文章的标题",
-      description: "文章的描述内容",
-      date: "创建的日期",
+      description: "ZeroTier是一款基于P2P的内网穿透工具，对于个人免费且支持最多100台设备端对端穿透，看上去很优秀。出于爱折腾的兴趣，就安装试用一下。首先在官网注册帐号并创建一个虚拟网，得到虚拟网ID。然后下载Windows安装包，在Windows上一路确定安装完毕，点击运行，右键托盘图标选择join，填写上新建的虚拟网ID，进行加入。",
+      date: "2021-04-27",
       link: "链接的地址",
-      img: "",
+      type: "javaScript",
+      img: require("../../static/images/1.jpeg"),
+    },
+    {
+      title: "文章的标题",
+      description: "ZeroTier是一款基于P2P的内网穿透工具，对于个人免费且支持最多100台设备端对端穿透，看上去很优秀。出于爱折腾的兴趣，就安装试用一下。首先在官网注册帐号并创建一个虚拟网，得到虚拟网ID。然后下载Windows安装包，在Windows上一路确定安装完毕，点击运行，右键托盘图标选择join，填写上新建的虚拟网ID，进行加入。",
+      date: "2021-04-27",
+      link: "链接的地址",
+      type: "java",
+      img: require("../../static/images/1.jpeg"),
+    },
+    {
+      title: "文章的标题",
+      description: "ZeroTier是一款基于P2P的内网穿透工具，对于个人免费且支持最多100台设备端对端穿透，看上去很优秀。出于爱折腾的兴趣，就安装试用一下。首先在官网注册帐号并创建一个虚拟网，得到虚拟网ID。然后下载Windows安装包，在Windows上一路确定安装完毕，点击运行，右键托盘图标选择join，填写上新建的虚拟网ID，进行加入。",
+      date: "2021-04-27",
+      link: "链接的地址",
+      type: "Node.js",
+      img: require("../../static/images/1.jpeg"),
+    },
+    {
+      title: "文章的标题",
+      description: "ZeroTier是一款基于P2P的内网穿透工具，对于个人免费且支持最多100台设备端对端穿透，看上去很优秀。出于爱折腾的兴趣，就安装试用一下。首先在官网注册帐号并创建一个虚拟网，得到虚拟网ID。然后下载Windows安装包，在Windows上一路确定安装完毕，点击运行，右键托盘图标选择join，填写上新建的虚拟网ID，进行加入。",
+      date: "2021-04-27",
+      link: "链接的地址",
+      type: "webpack",
+      img: require("../../static/images/1.jpeg"),
+    },
+    {
+      title: "文章的标题",
+      description: "ZeroTier是一款基于P2P的内网穿透工具，对于个人免费且支持最多100台设备端对端穿透，看上去很优秀。出于爱折腾的兴趣，就安装试用一下。首先在官网注册帐号并创建一个虚拟网，得到虚拟网ID。然后下载Windows安装包，在Windows上一路确定安装完毕，点击运行，右键托盘图标选择join，填写上新建的虚拟网ID，进行加入。",
+      date: "2021-04-27",
+      link: "链接的地址",
+      type: "javaScript",
+      img: require("../../static/images/1.jpeg"),
     }
   ]
   return (
       <Layout title="首页" description="首页">
-        <div className={ styles["motto"] }>
-          <p></p>
-          <p></p>
-        </div>
        <div className={ styles["home_content"] }>
-         <div className={ styles["home_item"] }>
-           <div className={ styles["title"] }></div>
-           <div className={ styles["description"] }></div>
-           <div className={ styles["options"] }></div>
-         </div>
-         <div className={ styles["home_item"] }>
-           <div className={ styles["title"] }></div>
-           <div className={ styles["description"] }></div>
-           <div className={ styles["options"] }></div>
-         </div>
-         <div className={ styles["home_item"] }>
-           <div className={ styles["title"] }></div>
-           <div className={ styles["description"] }></div>
-           <div className={ styles["options"] }></div>
-         </div>
-         <div className={ styles["home_item"] }>
-           <div className={ styles["title"] }></div>
-           <div className={ styles["description"] }></div>
-           <div className={ styles["options"] }></div>
+        {data.map((item, index) => {
+          return (
+            <div className={ styles["home_item"] }>
+              <div className={ styles["home_item_left"] }>
+                <div className={ styles["title"] }>{item.title}</div>
+                <div className={ styles["description"] }>{item.description}</div>
+                <div className={ styles["options"] }>
+                  <div className={ styles["time"] }>{item.date}</div>
+                  <div className={ styles["type"] }>{item.type}</div>
+                </div>
+              </div>
+              <div className={ styles["home_item_right"] }>
+                <img src={ item.img } alt=""/>
+              </div>
+            </div>
+          )
+        })}
+       </div>
+       <div className={ styles["home_footer"] }>
+         <div className={ styles["footer_content"] }>
+           <div className="motto">
+             <p></p>
+             <p></p>
+           </div>
          </div>
        </div>
       </Layout>
